ed9e65fd289767fb44b193a62b60dcd2592f36c9
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2022-10-08 17:39:48 +0500

    The script.js file and css have been styled

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector("#modal");
+const body = document.querySelector("body");
+
+function modalView() {
+  modal.style.display = "block";
+  body.style.opacity = 0.5;
+  modal.style.opacity = 1.0;
+}
+
+function exitBtn() {
+  modal.style.display  = 'none';
+  body.style.opacity = 1.0;
+}
